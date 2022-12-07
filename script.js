0d5cdb004613b8cffc95948ec2cb85ed94cce3d0
Aditya Kumar Sahu <aditya@acceldata.io>
2022-12-08 01:28:30 +0530

    code for all transitive dependencies only.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const {Builder, By, Key, Capabilities, until} = require('selenium-webdriver');
+const { Builder, By, Key, Capabilities, until } = require('selenium-webdriver');
 
 const chrome = require('selenium-webdriver/chrome');
 const options = new chrome.Options();
@@ -7,102 +7,116 @@ const options = new chrome.Options();
 
 const fs = require('fs');
 
-const data = 'write to a text file';
+var allRowsSelector = 'td.FIB tr.EK, td.FIB tr.DL';
 
 async function main() {
 
-  const caps = new Capabilities();
-  caps.setPageLoadStrategy('normal');
+    const caps = new Capabilities();
+    caps.setPageLoadStrategy('normal');
 
-  // launch browser
-  const driver = await new Builder().withCapabilities(caps).forBrowser(
-      'chrome').build();
+    // launch browser
+    const driver = await new Builder().withCapabilities(caps).forBrowser(
+        'chrome').build();
 
-  //navigate to webpage
-  await driver.get('https://acceldata.okta.com/login/login.htm');
+    //navigate to webpage
+    await driver.get('https://saas-eu.whitesourcesoftware.com/Wss/WSS.html#!product;id=288863');
 
-  //add search keyword
-  await driver
-  .findElement(By.id('okta-signin-username'))
-  .sendKeys('');
+    //add search keyword
+    setTimeout(async () => {
+        await driver.findElement(By.xpath('/html/body/div[4]/table/tbody/tr[2]/td/div[2]/table/tbody/tr/td[1]/table/tbody/tr[9]/td/a')).click();
 
-  await driver
-  .findElement(By.id('okta-signin-password'))
-  .sendKeys('', Key.RETURN);
+        await driver
+            .findElement(By.xpath('/html/body/div[4]/table/tbody/tr[2]/td/div[3]/table/tbody/tr/td/table/tbody/tr[4]/td/input'))
+            .sendKeys('aditya@acceldata.io');
 
-  setTimeout(async function () {
-    const authXpath = '/html/body/div[2]/div/main/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/span/input';
-    await driver
-    .findElement(By.xpath(authXpath))
-    .sendKeys(Key.RETURN);
+        await driver.findElement(By.xpath('/html/body/div[4]/table/tbody/tr[2]/td/div[3]/table/tbody/tr/td/table/tbody/tr[6]/td/button')).click();
 
-    fs.writeFile('output.txt', data, (err) => {
-      if (err) {
-        throw err;
-      }
-    })
+        setTimeout(async function () {
+            await driver
+                .findElement(By.id('okta-signin-username'))
+                .sendKeys('aditya@acceldata.io');
 
-    setTimeout(() => switchWindow(driver), 20000);
+            await driver
+                .findElement(By.id('okta-signin-password'))
+                .sendKeys('Aks@Mac@Ad@2201', Key.RETURN);
 
-  }, 10000)
+            fs.writeFile('output.txt', '', (err) => {
+                console.log(err);
+            });
+
+            setTimeout(async () => {
+
+                const recordsXpath = '/html/body/div[7]/div[1]/div/div[3]/div/div/table/tbody/tr/td/div/div/table/tbody/tr[3]/td/div[2]/div/div[4]/table/tbody/tr/td[3]/div';
+                const el = await driver.findElement(By.xpath(recordsXpath));
+                const elTxt = await el.getText();
+                const totalRows = +elTxt.split('of')[1].trim();
+                console.log(totalRows);
+                var iterations = Math.floor(totalRows / 100);
+
+                getTransitives(driver, iterations)
+            }, 30000);
+
+        }, 3000)
+
+
+    }, 6000)
 
 }
 
-async function getTransitives(driver) {
-  let results = [];
-  const transitiveLinkSelector = '.F2 .gwt-Anchor';
-  results = await driver.findElements(By.css(transitiveLinkSelector));
+async function getTransitives(driver, iterations) {
+    let results = [];
 
-  for (const result of results) {
-    const text = await result.getText();
-    const tag = await result.getTagName();
-    if (text === 'Transitive' && tag === 'a') {
-      const asd = await getDependencyName(result, driver);
-      console.log(asd);
-
-      // async function deps() {
-      //     const deps = await driver.findElements(By.css('.CY .gwt-Anchor'));
-      //     await driver.findElement(By.css('.gwt-DialogBox .gwt-Button')).click();
-      //     console.log('deps', deps);
-      // }
-      // await deps();
+    const transitiveLinkSelector = '.F2 .gwt-Anchor';
+    results = await driver.findElements(By.css(transitiveLinkSelector));
+    if (!results.length) {
+        console.log("done");
     }
-  }
-  // results.forEach(async (result) => {
+    for (const result of results) {
+        const text = await result.getText();
+        const tag = await result.getTagName();
+        if (text === 'Transitive' && tag === 'a') {
+            const asd = await getDependencyName(result, driver);
+        }
+    }
 
-  // })
+    if (iterations > 0) {
+        const nextPageSelector = 'img[aria-label=\"Next page\"]'
+        await driver.findElement(By.css(nextPageSelector)).click();
+        setTimeout(async () => {
+            getTransitives(driver, --iterations);
+        }, 3000);
+    }
 
-  if (!results.length) {
-    console.log("running again");
-    setTimeout(() => getTransitives(driver), 10000)
-  }
 }
 
 async function getDependencyName(result, driver) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      await result.click()
-      const deps = await driver.findElements(By.css('.CY .gwt-Anchor'));
-      await driver.findElement(By.css('.gwt-DialogBox .gwt-Button')).click();
-      console.log('deps', deps);
-      resolve(1);
-    } catch (e) {
-      reject(e);
-    }
+    return new Promise(async (resolve, reject) => {
+        try {
+            await result.click()
+            setTimeout(async () => {
+                const deps = await driver.findElements(By.css('.CY .gwt-Anchor'));
+                for (const d of deps) {
+                    const dep = await d.getText();
+                    console.log('deps', dep);
+                    fs.appendFile('output.txt', dep + '\n', (err) => {
+                        if (err) {
+                            throw err;
+                        }
+                    })
+                }
 
-  });
+                await driver.findElement(By.css('.gwt-DialogBox .gwt-Button')).click();
+
+                resolve(1);
+            }, 2000)
+
+        } catch (e) {
+            reject(e);
+        }
+
+    });
 }
 
-async function switchWindow(driver) {
-  const originalWindow = await driver.getWindowHandle();
-  const windows = await driver.getAllWindowHandles();
-  windows.forEach(async handle => {
-    if (handle !== originalWindow) {
-      driver.close();
-      await driver.switchTo().window(handle);
-      setTimeout(() => getTransitives(driver), 20000)
-    }
-  });
-}
+
 
 main();
